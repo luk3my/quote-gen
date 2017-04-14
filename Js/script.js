0fd7b218a57aeccce3a276b851bc98ffd73a75e5
Luke Young <Luke Young>
2017-04-15 08:26:38 +1000

    First commit

---
+++ Js/script.js
@@ -0,0 +1,43 @@
+$(document).ready(function() {
+  
+  var quote;
+  var author;
+  
+      function getNewQuote() {
+          $.ajax({
+            url: 'http://api.forismatic.com/api/1.0/',
+            jsonp: 'jsonp',
+            dataType: 'jsonp',
+            data: {
+              method: 'getQuote',
+              lang: 'en',
+              format: 'jsonp'
+            },
+            success: function(response) {
+             console.log(response.quoteText);
+              quote = response.quoteText;
+              author = response.quoteAuthor;
+              $('#quote').text(quote);
+              if (author) {
+                $('#author').text('-' + author);
+              }else{
+                $('#author').text('-unknown');
+                  }
+                }
+              });
+            }
+            getNewQuote();
+            
+            $('.get-quote').on('click', function(event) {
+              getNewQuote(event);
+              event.preventDefault();
+            });
+            
+			$('.share-quote').on('click', function(event) {
+				event.preventDefault();
+				window.open('https://twitter.com/intent/tweet?text=' + encodeURIComponent(quote + "- " + author));
+				
+			});
+
+			
+          });
